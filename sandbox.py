81fd1ca2a42fdecf516b7ef92b58a5cb31c373fd
m4tice <ductuan2908@gmail.com>
2025-11-29 17:36:34 +0700

    feat: clean sandbox

--- sandbox.py
+++ sandbox.py
@@ -2,13 +2,8 @@
 Sandbox
 """
 
-from mcp_project.utils.generic_utils import info, debug, error
-from mcp_project.paramdef_handler.paramdef_utils import get_definition_path_rapidfuzz
-
 def main():
-    keyword= "ComIpduDirection"
-    result = get_definition_path_rapidfuzz(keyword)
-    info(f"Results for keyword '{keyword}': {result[0]['file'].split('\\')[-1]}")
+    pass
 
 if __name__ == "__main__":
     main()
